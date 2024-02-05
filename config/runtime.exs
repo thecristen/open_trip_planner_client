ffbe84364d77d3d7d0e0c354904c02c65e116dc5
Cristen Jones <cjones3@mbta.com>
2024-02-05 14:32:28 -0500

    hotfix(config): check OTP_URL before using it

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,6 @@
 import Config
 
-if config_env() != :test do
+if config_env() != :test and System.get_env("OTP_URL", "") != "" do
   config :open_trip_planner_client,
     otp_url: System.get_env("OTP_URL")
 end
