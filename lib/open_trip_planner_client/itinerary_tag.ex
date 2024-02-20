280524b94de39f321fed5065044bbb0e99179ca6
Cristen Jones <cjones3@mbta.com>
2024-02-20 17:27:06 -0500

    feat: simple tiebreaker

--- lib/open_trip_planner_client/itinerary_tag.ex
+++ lib/open_trip_planner_client/itinerary_tag.ex
@@ -2,7 +2,7 @@ defmodule OpenTripPlannerClient.ItineraryTag do
   @moduledoc """
   Logic for a tag which can be applied to itineraries which are the best by some criterion.
   """
-  @tag_priority_order [:earliest_arrival, :most_direct, :least_walking, :shortest_trip]
+  @tag_priority_order [:earliest_arrival, :shortest_trip, :least_walking]
 
   @callback optimal :: :max | :min
   @callback score(map()) :: number() | nil
@@ -36,6 +36,7 @@ defmodule OpenTripPlannerClient.ItineraryTag do
     |> Enum.zip_with(scores, fn itinerary, score ->
       apply_best(itinerary, tag_module.tag(), score === best_score and not is_nil(score))
     end)
+    |> final_tiebreaker(tag_module.tag())
     |> Enum.sort(&tagged_first/2)
   end
 
@@ -54,6 +55,37 @@ defmodule OpenTripPlannerClient.ItineraryTag do
   end
 
   defp tag_priority(tag), do: Enum.find_index(@tag_priority_order, &(&1 == tag)) || 0
+
+  defp final_tiebreaker(itineraries, tag) do
+    indexed_itineraries =
+      itineraries
+      |> Enum.with_index()
+
+    tied_indexes =
+      indexed_itineraries
+      |> Enum.filter(fn {itinerary, _} ->
+        itinerary["tag"] == tag
+      end)
+      |> Enum.map(&elem(&1, 1))
+
+    if Enum.count(tied_indexes) <= 1 do
+      itineraries
+    else
+      winning_index = Enum.random(tied_indexes)
+
+      indexed_itineraries
+      |> Enum.map(&to_itinerary(&1, winning_index, tag))
+    end
+  end
+
+  defp to_itinerary({itinerary, index}, winning_index, _)
+       when index == winning_index,
+       do: itinerary
+
+  defp to_itinerary({%{"tag" => itinerary_tag} = itinerary, _}, _, tag) when itinerary_tag == tag,
+    do: %{itinerary | "tag" => nil}
+
+  defp to_itinerary({itinerary, _}, _, _), do: itinerary
 
   # Sort itineraries such that the tagged ones are always preceding untagged
   defp tagged_first(%{"tag" => nil}, %{"tag" => nil}), do: true
